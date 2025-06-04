b60d3662c3c3d8a281348878bc4ec5e3625256a9
NancyHouse3 <nancyehouse3@gmail.com>
2025-06-03 21:24:39 -0400

    Getting started
    I'd like to have some updates at the beginning and end of each dev session and maybe some in between. Here's some work I got to start with

---
+++ src/Centre.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Centre {
+    static Random random = new Random();
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+
+
+    }
+
+    static void banana() {
+        
+    }
+
+}
